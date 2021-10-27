de8e5eb13ffc5a6357e4611a4dfd20072c3e089f
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2021-10-27 16:55:55 +0500

    add token in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const { SignInValidation } = require("../modules/validations");
+const { createToken } = require("../modules/jwt");
 
 module.exports = class UserController {
     static async SignInController(req, res, next) {
@@ -11,10 +12,38 @@ module.exports = class UserController {
             const user = await req.db.users.findOne({
                 where: {
                     user_username: username,
-                }
+                },
+                raw: true,
             });
 
             console.log(user);
+
+            if (!user) throw new res.error(400, "User not found");
+
+            await req.db.session.destroy({
+                session_useragent: req.headers["user-agent"] || "Unknown",
+                user_id: user.user_id,
+            });
+
+            const session = await req.db.session.create({
+                session_useragent: req.headers["user-agent"] || "Unknown",
+                user_id: user.user_id,
+            }, {
+                raw: true,
+            });
+
+            const token = await createToken({
+                session_id: session.dataValues.session_id,
+            })
+
+            res.status(201).json({
+                ok: true,
+                message: "Token created seccessfully",
+                data: {
+                    token,
+                }
+            })
+            console.log(session);
         } catch (error) {
             console.log("SignInController Error: " + error + "");
             next(error);
