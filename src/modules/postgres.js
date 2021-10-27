ba4ad7c8cd366c76510a188cf24a32f8602d5c87
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2021-10-27 11:34:22 +0500

    add information postgres.js

--- src/modules/postgres.js
+++ src/modules/postgres.js
@@ -1,13 +1,23 @@
+const { build } = require("joi");
 const { Sequelize } = require("sequelize");
+const userModel = require("../models/userModel");
+const reletion = require("./reletion");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    logging: false, 
+    logging: false,
 });
 
 module.exports = async function postgres() {
     try {
         await sequelize.authenticate();
-        console.log(`the database is running`)
+
+        let db = {};
+
+        db.users = await userModel(sequelize, Sequelize);
+
+        reletion
+
+        return db;
     } catch (error) {
         console.log("postgres Error:" + error + "");
     }
