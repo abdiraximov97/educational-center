12cdd0fb2f80d186a9923790f59d473f9490c080
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2021-10-31 09:44:03 +0500

    userControllerga CreateUserController qo'shildi

--- src/routes/users/userRoute.js
+++ src/routes/users/userRoute.js
@@ -1,7 +1,8 @@
-const { SignInController } = require("../../controllers/userController");
+const { SignInController, CreateUserController } = require("../../controllers/userController");
 
 const userRoute = require("express").Router();
 
-userRoute.post("sign_in", SignInController);
+userRoute.post("/sign_in", SignInController);
+userRoute.post("/account", CreateUserController);
 
 module.exports = userRoute;
